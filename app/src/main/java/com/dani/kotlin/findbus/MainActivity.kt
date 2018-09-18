9ab4782720ebd0de8f583d48c64d8918d9dcc53e
Daniel Morato <daniel.morato.20@gmail.com>
2018-09-18 21:08:15 +0200

    update

--- app/src/main/java/com/dani/kotlin/findbus/MainActivity.kt
+++ app/src/main/java/com/dani/kotlin/findbus/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.dani.kotlin.findbus
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showMap: Button = findViewById(R.id.btn_ShowMap)
+        showMap.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            // intent.putExtra("key", value)
+            startActivity(intent)
+        }
     }
 }
